761788d19bddbf99b62d18f543a56681a81cb32a
ekklesia <ekklesiaendalkachew@gmail.com>
2025-12-20 21:43:14 +0300

    DB_Utils

---
+++ src/com/attendance/util/DBConnection.java
@@ -0,0 +1,14 @@
+package com.attendance.util;
+
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/attendance_db";
+    private static final String USER = "root";
+    private static final String PASS = "root";
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
