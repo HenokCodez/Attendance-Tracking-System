17c6b8f93fe24c662c75807a68d056acc7f56d4b
Meti1215 <metimengistu6@gmail.com>
2025-12-22 08:18:11 +0300

    Student_Model

---
+++ src/com/attendance/model/Student.java
@@ -0,0 +1,15 @@
+package com.attendance.model;
+
+public class Student {
+    private int id;
+    private String name;
+    private String rollNo;
+    public Student(int id, String name, String rollNo){
+        this.id = id;
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+    public int getId(){return id;}
+    public String getName(){return name;}
+    public String getRollNo(){return rollNo;}
+}
